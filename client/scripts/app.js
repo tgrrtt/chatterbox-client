709038b73deb7aeda891f53c3d2a0f8aaa5ee49a
tgrrtt <jtgarrett123@gmail.com>
2014-09-22 14:34:21 -0700

    Added empty methods to app

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,2 +1,46 @@
 // YOUR CODE HERE:
 
+var app = {
+  server: 'https://api.parse.com/1/classes/chatterbox',
+  init: function(){
+
+  },
+  send: function(message){
+    $.ajax({
+      url: this.server,
+      type: 'POST',
+      data: JSON.stringify(message),
+      contentType: 'application/json',
+      success: function (data) {
+        console.log(data);
+        console.log('chatterbox: Message sent');
+      },
+      error: function (data) {
+        console.log(data);
+        console.error('chatterbox: Failed to send message');
+      }
+    });
+  },
+  fetch: function(){
+    $.get(this.server,{
+      contentType: 'application/json'
+    }, function(data) {
+      console.log(data);
+    });
+  },
+  clearMessages: function(){
+
+  },
+  addMessage: function(message) {
+
+  },
+  addRoom: function(room) {
+
+  },
+  addFriend: function(friend) {
+
+  },
+  handleSubmit: function() {
+
+  }
+};
