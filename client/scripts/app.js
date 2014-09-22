eca496f425284f8ba1c6b0022573ff2c0b157396
tgrrtt <jtgarrett123@gmail.com>
2014-09-22 15:32:38 -0700

    Fetch working, addAllMessages, clearMessages added

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -22,17 +22,32 @@ var app = {
     });
   },
   fetch: function(){
-    $.get(this.server,{
-      contentType: 'application/json'
-    }, function(data) {
-      console.log(data);
+    var that = this;
+    $.ajax({
+      url: this.server,
+      type: 'GET',
+      contentType: 'application/json',
+      success: function (data) {
+        console.log('success');
+        that.addAllMessages(data.results);
+      },
+      error: function (data) {
+        console.error('chatterbox: failed to fetch');
+      }
     });
   },
   clearMessages: function(){
-
+    $('#chats').html('');
   },
   addMessage: function(message) {
-
+    var $message = $('<li></li>');
+    $message.html(message.username + ': ' + message.text);
+    $('#chats').append($message);
+  },
+  addAllMessages: function(allMessages) {
+    for (var i = 0; i < allMessages.length; i++) {
+      this.addMessage(allMessages[i]);
+    }
   },
   addRoom: function(room) {
 
@@ -44,3 +59,4 @@ var app = {
 
   }
 };
+app.fetch();
