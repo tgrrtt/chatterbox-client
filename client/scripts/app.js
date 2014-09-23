b40d7c81aa167a3d18db8c9ee5c1420950a6ff29
tgrrtt <jtgarrett123@gmail.com>
2014-09-22 19:32:28 -0700

    Add rooms

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -2,6 +2,8 @@
 $(document).ready(function(){
   window.app = {
     server: 'https://api.parse.com/1/classes/chatterbox',
+    currentRoom: "Lobby",
+    allRooms: [],
     init: function(){
       this.fetch();
       setInterval(this.fetch.bind(this), 1000);
@@ -10,10 +12,9 @@ $(document).ready(function(){
       if (message === undefined) {
         var message = {};
         message.text = $('.messageBox').val();
-        message.username = $('.username').val();
-        message.roomname = 'somewhere';
+        message.username = $('.usernameBox').val();
+        message.roomname = $(".roomnameBox").val() || "somewhere";
       }
-      console.log(message);
 
       $.ajax({
         url: this.server,
@@ -21,7 +22,7 @@ $(document).ready(function(){
         data: JSON.stringify(message),
         contentType: 'application/json',
         success: function (data) {
-          $('#message').val('');
+          $('.messageBox').val('');
           console.log('chatterbox: Message sent');
         },
         error: function (data) {
@@ -38,10 +39,11 @@ $(document).ready(function(){
         data: {
           order: '-createdAt'
         },
-        contentType: 'application/json',
+        contentType: 'application/json; charset=utf-8',
         success: function (data) {
           //console.log('success');
           that.addAllMessages(data.results);
+          that.addRooms(data.results);
         },
         error: function (data) {
           console.error('chatterbox: failed to fetch');
@@ -53,9 +55,14 @@ $(document).ready(function(){
     },
     addMessage: function(message) {
       if (message.text.length < 400) {
-        var $message = $('<li></li>');
-        $message.text(message.username + ': ' + message.text);
-        $('#chats').append($message);
+        var $messageHolder = $('<div></div>');
+        //console.log(message.username);
+        $username = $('<span class="username"></span>').text(message.username);
+        $msgText = $('<span></span>').text(': ' +  message.text);
+        //$message.text(message.username + ': ' + message.text);
+        $messageHolder.append($username);
+        $messageHolder.append($msgText);
+        $('#chats').append($messageHolder);
       }
     },
     addAllMessages: function(allMessages) {
@@ -64,13 +71,16 @@ $(document).ready(function(){
         this.addMessage(allMessages[i]);
       }
     },
-    addRoom: function(room) {
-
+    addRooms: function(data) {
+      for (var i = 0; i < data.length; i++) {
+        var name = data[i].roomname;
+        if (this.allRooms.indexOf(name) === -1 && name !== undefined) {
+          this.allRooms.push(name);
+          $('#roomSelect').append('<option value='+name+'>'+name+'</option>');
+        }
+      }
     },
     addFriend: function(friend) {
-
-    },
-    handleSubmit: function() {
 
     }
   };
