38221b0c0bb43a402baada9d6d9a451f7b68b67f
tgrrtt <jtgarrett123@gmail.com>
2014-09-23 11:25:39 -0700

    Fetch messages from server

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,29 +1,51 @@
 var Message = Backbone.Model.extend({
-  defaults: {
-    'roomname': 'Lobby'
-  },
-  initialize: function(username, message, roomname) {
-    this.set('username', username);
-    this.set('message', message);
-    if (roomname) {this.set('roomname', roomname);}
+  initialize: function(data) {
+    //console.log(data);
+    if (!data.username) {this.set('username', 'Anon');}
+    if (!data.message) {this.set('message', '#YOLO');}
+    if (!data.roomname) {this.set('roomname', 'Lobby');}
+    //console.log(this.get('username'));
   }
 });
 
 var Messages = Backbone.Collection.extend({
   model: Message,
-  url: 'https://api.parse.com/1/classes/chatterbox'
+  url: 'https://api.parse.com/1/classes/chatterbox',
+  initialize: function() {
+    this.fetch({
+      data: {
+        order: '-createdAt'
+      },
+      success: function(collection, response, options) {
+        //console.log(response, "response");
+      }
+    });
+  },
+  parse: function(response) {
+    // if it's like {data: [model1, model2, model3...]}
+    return response.results;
+  }
 });
 
 
 
 var MessagesView = Backbone.View.extend({
   initialize: function(){
-
   },
   render: function(){}
 });
 
 
-var myMessage = new Message('nick', 'test');
-var myMessage2 = new Message('nick', 'test', 'other room');
+// var myMessage = new Message('nick', 'test');
+// var myMessage2 = new Message('nick', 'test', 'other room');
 
+var messages = new Messages();
+
+// messages.fetch({
+//   data: {
+//     order: '-createdAt'
+//   },
+//   success: function(collection, response, options) {
+//     console.log(response, "response");
+//   }
+// });
