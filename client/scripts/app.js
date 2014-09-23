6ac1c05ffa9500639a0e76078f4581584436f847
tgrrtt <jtgarrett123@gmail.com>
2014-09-23 11:01:18 -0700

    Started backbone app

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,95 +1,29 @@
-// YOUR CODE HERE:
-$(document).ready(function(){
-  window.app = {
-    server: 'https://api.parse.com/1/classes/chatterbox',
-    currentRoom: "Lobby",
-    allRooms: [],
-    init: function(){
-      this.fetch();
-      setInterval(this.fetch.bind(this), 1000);
-    },
-    send: function(message){
-      if (message === undefined) {
-        var message = {};
-        message.text = $('.messageBox').val();
-        message.username = $('.usernameBox').val();
-        message.roomname = $(".roomnameBox").val() || "somewhere";
-      }
-
-      $.ajax({
-        url: this.server,
-        type: 'POST',
-        data: JSON.stringify(message),
-        contentType: 'application/json',
-        success: function (data) {
-          $('.messageBox').val('');
-          console.log('chatterbox: Message sent');
-        },
-        error: function (data) {
-          console.log(data);
-          console.error('chatterbox: Failed to send message');
-        }
-      });
-    },
-    fetch: function(){
-      var that = this;
-      $.ajax({
-        url: this.server,
-        type: 'GET',
-        data: {
-          order: '-createdAt'
-        },
-        contentType: 'application/json; charset=utf-8',
-        success: function (data) {
-          that.addAllMessages(data.results);
-          that.addRooms(data.results);
-        },
-        error: function (data) {
-          console.error('chatterbox: failed to fetch');
-        }
-      });
-    },
-    clearMessages: function(){
-      $('#chats').html('');
-    },
-    addMessage: function(message) {
-      if (message.text.length < 400) {
-        var $messageHolder = $('<div></div>');
-        $username = $('<span class="username"></span>').text(message.username);
-        $msgText = $('<span></span>').text(': ' +  message.text);
-        $messageHolder.append($username);
-        $messageHolder.append($msgText);
-        $('#chats').append($messageHolder);
-      }
-    },
-    addAllMessages: function(allMessages) {
-      this.clearMessages();
-      for (var i = 0; i < 20; i++) {
-        this.addMessage(allMessages[i]);
-      }
-    },
-    addRooms: function(data) {
-      for (var i = 0; i < data.length; i++) {
-        var name = data[i].roomname;
-        if (this.allRooms.indexOf(name) === -1 && name !== undefined) {
-          this.allRooms.push(name);
-          $('#roomSelect').append('<option value='+name+'>'+name+'</option>');
-        }
-      }
-    },
-    addFriend: function(friend) {
-
-    }
-  };
-
-  app.init();
-
-
-  $('.send').on('click', function(){
-    app.send();
-  });
-
-
-
-
+var Message = Backbone.Model.extend({
+  defaults: {
+    'roomname': 'Lobby'
+  },
+  initialize: function(username, message, roomname) {
+    this.set('username', username);
+    this.set('message', message);
+    if (roomname) {this.set('roomname', roomname);}
+  }
 });
+
+var Messages = Backbone.Collection.extend({
+  model: Message,
+  url: 'https://api.parse.com/1/classes/chatterbox'
+});
+
+
+
+var MessagesView = Backbone.View.extend({
+  initialize: function(){
+
+  },
+  render: function(){}
+});
+
+
+var myMessage = new Message('nick', 'test');
+var myMessage2 = new Message('nick', 'test', 'other room');
+
